df0798f655854557370cdf65643fcc6a0d004de5
Aung Pyae Phyo <aungpyaephyo@mm-digital-solutions.com>
2019-05-30 23:59:31 +0630

    in-code-four.

--- app/src/main/java/com/padcmyanmar/rxjava/activities/MainActivity.kt
+++ app/src/main/java/com/padcmyanmar/rxjava/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.padcmyanmar.rxjava.activities
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
@@ -8,15 +9,14 @@ import android.view.MenuItem
 import com.padcmyanmar.rxjava.R
 import com.padcmyanmar.rxjava.RxJavaApp
 import com.padcmyanmar.rxjava.network.responses.RestaurantListResponse
-import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.Scheduler
+import io.reactivex.*
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.annotations.NonNull
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Callable
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +31,10 @@ class MainActivity : AppCompatActivity() {
 
         btnInCodeThree.setOnClickListener {
             networkCallWithRxJava()
+        }
+
+        btnInCodeFour.setOnClickListener {
+            longRunningOperationWithRxJava()
         }
     }
 
@@ -166,8 +170,52 @@ class MainActivity : AppCompatActivity() {
             })
     }
 
+    private fun longRunningOperationWithRxJava() {
+        tvText.text = ""
+        val integerSingle = Single.fromCallable(Callable<Int> { operation() })
+
+        integerSingle
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : SingleObserver<Int> {
+                override fun onSubscribe(@NonNull d: Disposable) {
+
+                }
+
+                override fun onSuccess(integer: Int) {
+                    tvText.text = "${tvText.text}\nOperation has finished. The value is $integer\n"
+                }
+
+                override fun onError(@NonNull e: Throwable) {
+
+                }
+            })
+
+        recursiveLogger(1)
+    }
+
     private fun getRestaurantListResponseObservable(): Observable<RestaurantListResponse> {
         val rxJavaApp: RxJavaApp = application as RxJavaApp
         return rxJavaApp.theApi.getRestaurantList()
+    }
+
+    private fun operation(): Int {
+        try {
+            // "Simulate" the delay.
+            Thread.sleep(5000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+        val result = 2345 + 1234
+        return result
+    }
+
+    private fun recursiveLogger(index: Int) {
+        Handler().postDelayed({
+            tvText.text = "${tvText.text} + Time waited  $index ) ${(index + 500)} \n"
+            if (index < 20) {
+                recursiveLogger(index + 1)
+            }
+        }, 500)
     }
 }
